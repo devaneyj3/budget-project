3023ecdade4ef2eab16f60eb3f678969a0924b06
Jordan Devaney <jordandevaney28@gmail.com>
2019-12-25 21:56:21 -0500

    can't append more numbers to display budget

--- index.js
+++ index.js
@@ -1,21 +1,27 @@
 //jshint esversion: 6
 const submitItem = document.querySelector(".budget-item-submit");
+let totalExpenses = 0;
 
 submitItem.addEventListener('click', function() {
     const option = document.querySelector('.option');
     const input = document.querySelector('.input-num');
     const displayExpense = document.querySelector('.exp-output');
     const displayIncome = document.querySelector('.inc-output');
+    
+
     if(option.value === 'exp') {
-        addToDisplay(input, displayExpense);
+        addToDisplay(displayExpense, input);
     }
     else {
-        addToDisplay(input, displayIncome);
+        addToDisplay(displayIncome, input);
     }
 });
 
-function addToDisplay(el, DOMEl) {
-
-    //add to main display of html
-    DOMEl.append(el.value);
+function addToDisplay(el, input) {
+    if( el.textContent === '0') {
+        el.textContent = input.value;
+    } else {
+        //ele empty update value
+        // updateExpenses();
+    }
 }
