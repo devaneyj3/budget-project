0b86bd0a4eed24e5bfd26577d32c3d10b2610459
Jordan Devaney <jordandevaney28@gmail.com>
2019-12-31 12:59:11 -0500

    format income

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ const submitItem = document.querySelector(".budget-item-submit");
 let totalExpenses = 0;
 let totalIncome = 0;
 
-submitItem.addEventListener('click', function() {
+submitItem.addEventListener('click', function () {
     const option = document.querySelector('.option');
     const input = document.querySelector('.input-num');
     const displayExpense = document.querySelector('.exp-output');
     const displayIncome = document.querySelector('.inc-output');
-    
 
-    if(option.value === 'exp') {
+
+    if (option.value === 'exp') {
         addToDisplay(displayExpense, input);
         updateExpenses(input, displayExpense);
         clearFields(input);
@@ -23,7 +23,7 @@ submitItem.addEventListener('click', function() {
 });
 
 function addToDisplay(el, input) {
-    if( el.textContent === '0') {
+    if (el.textContent === '0') {
         el.textContent = input.value;
     } else {
         //else add individual button items to ul
@@ -33,14 +33,27 @@ function addToDisplay(el, input) {
 
 function updateExpenses(input, el) {
     totalExpenses += parseInt(input.value);
-    el.textContent = totalExpenses;
+    // format the updated expenses with comma
+    let formatedExpense = formatWithCommas(totalExpenses.toString());
+    el.textContent = formatedExpense;
 }
 
 function updateIncome(input, el) {
     totalIncome += parseInt(input.value);
-    el.textContent = totalIncome;
+    // format the updated expenses with comma
+    let formatedIncome = formatWithCommas(totalIncome.toString());
+    el.textContent = formatedIncome;
 }
 
 function clearFields(el) {
     el.value = ' ';
+}
+
+function formatWithCommas(value, decimalIndex) {
+    let count = 0;
+    let readableNum = "";
+    decimalIndex = (decimalIndex === -1) ? undefined : decimalIndex;
+    let decimalPlaces = value.substr(decimalIndex);
+    value.substring(0, decimalIndex).split("").reverse().forEach((element) => { if (count === 3 && value.substring(0, decimalIndex).length > 3 && element !== "-") { readableNum += `,${element}`; count = 0; } else { readableNum += element; } count++; }); readableNum = readableNum.split("").reverse().join("");
+    if (decimalIndex > -1) { readableNum += decimalPlaces; } return readableNum;
 }
